1979155c4344924248d775bdffb31b4187d47d0e
K-S-7060319454 <rahulmca1993@gmail.com>
2026-06-30 23:49:09 +0530

    my first flask program with html,python

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
-print("helllo")
+from flask import Flask, render_template
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html") 
+if __name__ == "__main__":
+    app.run(debug=True)
